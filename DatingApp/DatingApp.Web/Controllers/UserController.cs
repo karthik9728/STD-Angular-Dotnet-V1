ae3b99e0e9a7ab1afe2273451c00b5ffb5432ce8
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-09 14:07:55 +0530

    API Project Setup and Basic User API

---
+++ DatingApp/DatingApp.Web/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x=>x.Id == id);
+
+            return Ok(user);
+        }
+    }
+}
---
+++ DatingApp/DatingApp.Web/Program.cs
@@ -0,0 +1,53 @@
+using DatingApp.Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#region CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CustomPolicy", x =>
+    {
+        x.WithOrigins("https://locahost:4200");
+        x.AllowAnyHeader();
+        x.AllowAnyMethod();
+    });
+});
+
+#endregion
+
+
+#region Database
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CustomPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
