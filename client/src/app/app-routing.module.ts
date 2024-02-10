f8829a55b47b87525fd40558df6040fbb44db591
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-11 00:47:28 +0530

    Basic Routing Setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: HomeComponent,
   },
   {
-    path: 'register',
-    component: RegisterComponent,
+    path: 'members',
+    component: MemberListComponent,
+  },
+  {
+    path: 'members/:id',
+    component: MemberDetailComponent,
+  },
+  {
+    path: 'lists',
+    component: ListsComponent,
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+    pathMatch: 'full',
   },
 ];
 
