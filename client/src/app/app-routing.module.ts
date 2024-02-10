fb5dc51a3d0540d1dca8d30717f3684197d37292
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-10 17:36:12 +0530

    Register Form UI

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AppComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
 
