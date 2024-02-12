8ddc2bd4d4143a9b1d7f089746c6278cdb4f82e0
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-12 15:43:09 +0530

    Auto Mapper Changes

--- DatingApp/DatingApp.Application/Common/MappingProfile.cs
+++ DatingApp/DatingApp.Application/Common/MappingProfile.cs
@@ -14,7 +14,8 @@ namespace DatingApp.Application.Common
     {
         public MappingProfile()
         {
-            CreateMap<AppUser,AppUserDto>().ReverseMap();
+            CreateMap<AppUser, AppUserDto>()
+                    .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<Photo,PhotoDto>().ReverseMap();
         }
     }
--- DatingApp/DatingApp.Application/DTO/User/AppUserDto.cs
+++ DatingApp/DatingApp.Application/DTO/User/AppUserDto.cs
@@ -14,6 +14,8 @@ namespace DatingApp.Application.DTO.User
 
         public string UserName { get; set; }
 
+        public string PhotoUrl { get; set; }
+
         public int Age { get; set; }
 
         public string KnownAs { get; set; }
