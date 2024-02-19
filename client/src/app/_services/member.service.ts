afde66609d4c7eb9631da5e7e5e365cf506ce9a2
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-20 05:06:09 +0530

    Pagination on UI

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -1,25 +1,47 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { map, of } from 'rxjs';
+import { PaginatedResult } from '../_models/pagination';
 
 @Injectable({ providedIn: 'root' })
 export class MemberService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) {}
 
-  getMembers() {
-    if (this.members.length > 0) return of(this.members);
+  getMembers(page?: number, itemsPerPage?: number) {
+    //if (this.members.length > 0) return of(this.members);
 
-    return this.http.get<Member[]>(this.baseUrl + 'user').pipe(
-      map((members) => {
-        this.members = members;
-        return members;
+    let params = new HttpParams();
+
+    if (page && itemsPerPage) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http
+      .get<Member[]>(this.baseUrl + 'user', {
+        observe: 'response',
+        params,
       })
-    );
+      .pipe(
+        map((response) => {
+          if (response.body) {
+            this.paginatedResult.result = response.body;
+          }
+
+          const pagination = response.headers.get('Pagination');
+          if (pagination) {
+            this.paginatedResult.pagination = JSON.parse(pagination);
+          }
+
+          return this.paginatedResult;
+        })
+      );
   }
 
   getMember(username: string) {
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MemberService } from '../../_services/member.service';
 import { Member } from '../../_models/member';
 import { Observable } from 'rxjs';
+import { Pagination } from '../../_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -9,11 +10,33 @@ import { Observable } from 'rxjs';
   styleUrl: './member-list.component.css',
 })
 export class MemberListComponent {
-  members$: Observable<Member[]> | undefined;
+  //members$: Observable<Member[]> | undefined;
+  members: Member[] | undefined;
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MemberService) {}
 
   ngOnInit() {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      },
+    });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber != event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
