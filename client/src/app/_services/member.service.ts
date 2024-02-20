cb2e057cfd9e139e77d4bd6e3b847d83599b1f0b
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-20 07:32:31 +0530

    Age & Gender Filter in UI

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -4,44 +4,59 @@ import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { map, of } from 'rxjs';
 import { PaginatedResult } from '../_models/pagination';
+import { UserParams } from '../_models/userParams';
 
 @Injectable({ providedIn: 'root' })
 export class MemberService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) {}
 
-  getMembers(page?: number, itemsPerPage?: number) {
-    //if (this.members.length > 0) return of(this.members);
+  getMembers(userParams: UserParams) {
+    let params = this.getPaginationHeader(
+      userParams.pageNumber,
+      userParams.pageSize
+    );
 
-    let params = new HttpParams();
+    params = params.append('minAge', userParams.minAge);
+    params = params.append('maxAge', userParams.maxAge);
+    params = params.append('gender', userParams.gender);
 
-    if (page && itemsPerPage) {
-      params = params.append('pageNumber', page);
-      params = params.append('pageSize', itemsPerPage);
-    }
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'user', params);
+  }
+
+  private getPaginatedResult<T>(url: string, params: HttpParams) {
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
 
     return this.http
-      .get<Member[]>(this.baseUrl + 'user', {
+      .get<T>(url, {
         observe: 'response',
         params,
       })
       .pipe(
         map((response) => {
           if (response.body) {
-            this.paginatedResult.result = response.body;
+            paginatedResult.result = response.body;
           }
 
           const pagination = response.headers.get('Pagination');
           if (pagination) {
-            this.paginatedResult.pagination = JSON.parse(pagination);
+            paginatedResult.pagination = JSON.parse(pagination);
           }
 
-          return this.paginatedResult;
+          return paginatedResult;
         })
       );
+  }
+
+  getPaginationHeader(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+
+    return params;
   }
 
   getMember(username: string) {
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { MemberService } from '../../_services/member.service';
 import { Member } from '../../_models/member';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Pagination } from '../../_models/pagination';
+import { UserParams } from '../../_models/userParams';
+import { User } from '../../_models/user';
+import { AccountService } from '../../_services/account.service';
 
 @Component({
   selector: 'app-member-list',
@@ -13,17 +16,31 @@ export class MemberListComponent {
   //members$: Observable<Member[]> | undefined;
   members: Member[] | undefined;
   pagination: Pagination | undefined;
-  pageNumber = 1;
-  pageSize = 5;
+  userParams: UserParams | undefined;
+  user: User;
 
-  constructor(private memberService: MemberService) {}
+  constructor(
+    private memberService: MemberService,
+    private accountService: AccountService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) {
+          this.userParams = new UserParams(user);
+          this.user = user;
+        }
+      },
+    });
+  }
 
   ngOnInit() {
     this.loadMembers();
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    if (!this.userParams) return;
+
+    this.memberService.getMembers(this.userParams).subscribe({
       next: (response) => {
         if (response.result && response.pagination) {
           this.members = response.result;
@@ -34,8 +51,15 @@ export class MemberListComponent {
   }
 
   pageChanged(event: any) {
-    if (this.pageNumber != event.page) {
-      this.pageNumber = event.page;
+    if (this.userParams && this.userParams.pageNumber != event.page) {
+      this.userParams.pageNumber = event.page;
+      this.loadMembers();
+    }
+  }
+
+  resetFilters() {
+    if (this.user) {
+      this.userParams = new UserParams(this.user);
       this.loadMembers();
     }
   }
