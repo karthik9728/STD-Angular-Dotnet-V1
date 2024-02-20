68c2492aa5b944052abbf0424698617cca63901f
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-02-20 08:31:03 +0530

    Claims GetUserId

--- DatingApp/DatingApp.Web/Extensions/ClaimsPrincipalExtentions.cs
+++ DatingApp/DatingApp.Web/Extensions/ClaimsPrincipalExtentions.cs
@@ -6,7 +6,14 @@ namespace DatingApp.Web.Extensions
     {
         public static string GetUserName(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            var userId = Int32.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            return userId;
         }
     }
 }
--- DatingApp/DatingApp.Web/Helpers/LogUserActivity.cs
+++ DatingApp/DatingApp.Web/Helpers/LogUserActivity.cs
@@ -12,11 +12,11 @@ namespace DatingApp.Web.Helpers
 
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username = resultContext.HttpContext.User.GetUserName();
+            var userId = resultContext.HttpContext.User.GetUserId();
 
             var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
 
-            var user = await repo.GetUserByUsernameAsync(username);
+            var user = await repo.GetUserByIdAsync(userId);
 
             user.LastActive = DateTime.UtcNow;
 
